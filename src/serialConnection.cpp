35f17a4d929460b4b3704598d66d6b10739d71a6
Zak Henry <zak.henry@gmail.com>
2011-12-10 23:11:35 +1300

    rename serialConnection class

--- src/serialConnection.cpp
+++ src/serialConnection.cpp
@@ -1,55 +1,66 @@
 //
-//  stepperControl.cpp
+//  SerialConnection.cpp
 //  Delta Sim
 //
 //  Created by Zak Henry on 9/06/11.
 //  Copyright 2011 __MyCompanyName__. All rights reserved.
 //
 
-#include "stepperControl.h"
+#include "SerialConnection.h"
 
 
-StepperControl::StepperControl(){ //constructor
+SerialConnection::SerialConnection(){ //constructor
     
 	cout << "Stepper control class instantiated \n";
     
     stepper0Ready = stepper1Ready = stepper2Ready = true;
     stepper0Connected = stepper1Connected = stepper2Connected = false;
-    cout <<"stepperControl constructor complete\n";
+    cout <<"SerialConnection constructor complete\n";
 
 }
 
-void StepperControl::setupDevices(){
+void SerialConnection::setupDevices(){
     
     serial0.enumerateDevices();
     printf("enumerated serial0 devices\n");
     
-    if(!serial0.setup(/*"tty.usbserial-A9007Mbm"*/5, 9600)){
-        printf("Serial setup failed!\n");
+//    if(!serial0.setup(/*"tty.usbserial-A9007Mbm"*/5, 9600)){
+    if(!serial0.setup("/dev/tty.usbserial-A4005jpk", 9600)){
+        printf("Serial0 setup failed!\n");
     }else{
         stepper0Connected = true;
     }
     
-    if(!serial1.setup(/*"tty.usbserial-A9007Mbm"*/7, 9600)){
-        printf("Serial setup failed!\n");
+//    if(!serial1.setup(/*"tty.usbserial-A9007Mbm"*/7, 9600)){
+    if(!serial1.setup("/dev/tty.usbserial-A4005jpl", 9600)){
+        printf("Serial1 setup failed!\n");
     }else{
         stepper1Connected = true;
     }
     
-    if(!serial2.setup(/*"tty.usbserial-A9007Mbm"*/9, 9600)){
-        printf("Serial setup failed!\n");
+//    if(!serial2.setup(/*"tty.usbserial-A9007Mbm"*/9, 9600)){
+    if(!serial2.setup("/dev/tty.usbserial-A4005jpn", 9600)){
+        printf("Serial2 setup failed!\n");
     }else{
         stepper2Connected = true;
     }
     
-    cout <<"stepperControl setup complete\n";
+    cout <<"SerialConnection setup complete\n";
 }
 
-bool StepperControl::println(int stepper, string line){
+bool SerialConnection::println(int stepper, string line){
+    
+    line += "\n";
     
     unsigned char * charLine = new unsigned char[line.size()+1];
     charLine[line.size()]=0;
     memcpy(charLine,line.c_str(),line.size());
+    
+    serialMessage newMessage;
+    
+    newMessage.time = ofToString(ofGetHours())+":"+ofToString(ofGetMinutes())+":"+ofToString(ofGetSeconds());
+    newMessage.input = false;
+    newMessage.message = line;
     
     switch (stepper) {
         case 0:
@@ -57,6 +68,7 @@ bool StepperControl::println(int stepper, string line){
             if (stepper0Connected){
                 stepper0Ready = false;
                 serial0.writeBytes(charLine,line.size());
+                stepper0io.push_back(newMessage);
                 cout <<"output to serial0: "<<charLine<<"\n";
             }
             
@@ -68,6 +80,7 @@ bool StepperControl::println(int stepper, string line){
             if (stepper1Connected){
                 stepper1Ready = false;
                 serial1.writeBytes(charLine,line.size());
+                stepper1io.push_back(newMessage);
                 cout <<"output to serial1: "<<charLine<<"\n";
             }
         }
@@ -78,6 +91,7 @@ bool StepperControl::println(int stepper, string line){
             if (stepper2Connected){
                 stepper2Ready = false;
                 serial2.writeBytes(charLine,line.size());
+                stepper2io.push_back(newMessage);
                 cout <<"output to serial2: "<<charLine<<"\n";
             }
         }
@@ -88,26 +102,27 @@ bool StepperControl::println(int stepper, string line){
     }
 }
 
-void StepperControl::setStepper(int stepper, float angle, float speed){
+void SerialConnection::setStepper(int stepper, float angle, float speed){
     
     char buffer [50];
-    sprintf (buffer, "d%f-s%de", angle+90, (int)speed); //note speed is cast to int for now, will be float eventually. angle has 90 added to it so it can be in the range 0-180
+//    sprintf (buffer, "d%f-s%de", angle+90, (int)speed); //note speed is cast to int for now, will be float eventually. angle has 90 added to it so it can be in the range 0-180
+    sprintf(buffer, "%f\n%f", angle, speed);
 //    sprintf (buffer, "$"); //note speed is cast to int for now, will be float eventually
     println (stepper, buffer);
     
 }
 
-void StepperControl::update(){
+void SerialConnection::update(){
     
-    if (!robotReadyForData()&&steppersConnected()){ //only read if a message has been sent to steppers, and all are connected
+//    if (!robotReadyForData()&&steppersConnected()){ //only read if a message has been sent to steppers, and all are connected
         read(serial0, 0, bytesReturned0, messageBuffer0, message0);
         read(serial1, 1, bytesReturned1, messageBuffer1, message1);
         read(serial2, 2, bytesReturned2, messageBuffer2, message2);
-    }
+//    }
 
 }
 
-bool StepperControl::steppersConnected(){
+bool SerialConnection::steppersConnected(){
     
     
     if (stepper0Connected && stepper1Connected && stepper2Connected){
@@ -118,7 +133,7 @@ bool StepperControl::steppersConnected(){
     
 }
 
-bool StepperControl::robotReadyForData(){
+bool SerialConnection::robotReadyForData(){
     
     
     if (steppersConnected() && stepper0Ready && stepper1Ready && stepper2Ready){
@@ -129,7 +144,7 @@ bool StepperControl::robotReadyForData(){
     
 }
 
-void StepperControl::read(ofSerial& serialPort, int stepperNum, unsigned char * bytesReturned, string& messageBuffer, string& message)
+void SerialConnection::read(ofSerial& serialPort, int stepperNum, unsigned char * bytesReturned, string& messageBuffer, string& message)
 {
     
     // if we've got new bytes
@@ -151,9 +166,9 @@ void StepperControl::read(ofSerial& serialPort, int stepperNum, unsigned char * 
                 message = messageBuffer;
                 messageBuffer = "";
                 
-                if (message == "done"){
+                if (message == "READY"){
                     message = "";
-                    cout << "stepper"<<stepperNum<<" is done\n";
+                    cout << "stepper"<<stepperNum<<" is ready\n";
                     switch (stepperNum) {
                         case 0:
                             stepper0Ready = true;
@@ -171,7 +186,30 @@ void StepperControl::read(ofSerial& serialPort, int stepperNum, unsigned char * 
                             break;
                     }
                 }else {
-                    cout <<"UNEXPECTED MESSAGE ("<<message<<")\n";
+                    
+                    serialMessage newMessage;
+                    
+                    newMessage.time = ofToString(ofGetHours())+":"+ofToString(ofGetMinutes())+":"+ofToString(ofGetSeconds());
+                    newMessage.input = true;
+                    newMessage.message = message;
+                    
+                    switch (stepperNum) {
+                        case 0:
+                            stepper0io.push_back(newMessage);
+                            break;
+                            
+                        case 1:
+                            stepper1io.push_back(newMessage);
+                            break;
+                            
+                        case 2:
+                            stepper2io.push_back(newMessage);
+                            break;
+                            
+                        default:
+                            break;
+                    }
+                    
                 }
                 
                 break;
@@ -181,7 +219,7 @@ void StepperControl::read(ofSerial& serialPort, int stepperNum, unsigned char * 
                 if(*bytesReturned != '\r')
                     messageBuffer += *bytesReturned;
             }
-            cout << "  messageBuffer: (" << messageBuffer << ") steppernumber:"<<stepperNum<<"\n";
+//            cout << "  messageBuffer: (" << messageBuffer << ") steppernumber:"<<stepperNum<<"\n";
         }
         
         // clear the message buffer
--- src/serialConnection.h
+++ src/serialConnection.h
@@ -1,19 +1,25 @@
 //
-//  stepperControl.h
+//  SerialConnection.h
 //  Delta Sim
 //
 //  Created by Zak Henry on 9/06/11.
 //  Copyright 2011 __MyCompanyName__. All rights reserved.
 //
-#ifndef _STEPPER_CONTROL // if this class hasn't been defined, the program can define it
-#define _STEPPER_CONTROL // by using this if statement you prevent the class to be called more 
+#ifndef _SERIAL_CONNECTION // if this class hasn't been defined, the program can define it
+#define _SERIAL_CONNECTION // by using this if statement you prevent the class to be called more 
 // than once which would confuse the compiler
 
 #include "ofMain.h"
 
 #define NUM_BYTES 1
 
-class StepperControl {
+struct serialMessage {
+    string time;
+    string message;
+    bool input;
+};
+
+class SerialConnection {
     
     vector<char>buffer;
     
@@ -22,7 +28,7 @@ class StepperControl {
     
 public:
     
-    StepperControl(); //constructor
+    SerialConnection(); //constructor
     void setupDevices();
     
     ofSerial serial0, serial1, serial2;
@@ -39,6 +45,8 @@ public:
     string message0, message1, message2, messageBuffer0, messageBuffer1, messageBuffer2;
     unsigned char bytesReturned0[1], bytesReturned1[1], bytesReturned2[1];
     void read(ofSerial& serialPort, int portNum, unsigned char * bytesReturned, string& messageBuffer, string& message);
+    
+    vector<serialMessage>stepper0io, stepper1io, stepper2io;
     
 };
 
